a06eb1ca9173152fe0ff8fec5516b5659ab244aa
SHREYANSH-MALLIK <86109327+SHREYANSH-MALLIK@users.noreply.github.com>
2022-10-12 19:11:53 +0530

    Add files via upload

--- Project 130/main.py
+++ Project 130/main.py
@@ -1,27 +1,12 @@
 import pandas as pd
 import csv
 
-df = pd.read_csv("main.csv")
+df = pd.read_csv("dwarf_stars.csv")
 print(df.shape)
-
-del df['pl_orbpererr1']
-del df['pl_orbpererr2']
 
 print(df.shape)
 
 print(list(df))
-
-df = df.rename({
-    'pl_hostname' : 'solar_system_name',
-    'pl_discmethod' : 'planet_discovery_method',
-    'pl_orbincl' : 'planet_orbital_inclination',
-    'pl_dens' : 'planet_density',
-    'ra_str' : 'right_ascension',
-    'dec_str' : 'declination',
-    'st_teff' : 'host_temperature',
-    'st_mass' : 'host_mass',
-    'st_rad' : 'host_radius'
-},axis = "columns")
 
 print(list(df))
 
